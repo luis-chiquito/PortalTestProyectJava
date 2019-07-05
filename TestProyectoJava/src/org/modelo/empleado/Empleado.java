179f7c6565f5604c2753be0e17b2454c79ed65e8
chiki <chiki@DESKTOP-MEBMUMH.mshome.net>
2019-07-05 12:22:40 -0500

    3

--- TestProyectoJava/src/org/modelo/empleado/Empleado.java
+++ TestProyectoJava/src/org/modelo/empleado/Empleado.java
@@ -2,5 +2,10 @@ package org.modelo.empleado;
 
 public class Empleado {
 
-	
+	public static void main(String[] args ) {
+		//system.out.println("este es un mensaje ");
+		System.out.println("ESTE ES UN COMENTARIO");
+		
+		
+	} 
 }
